0381591660fdc19ffee79b88bebf835eabbc7004
Teddybiovlsi <m11113005@yuntech.edu.tw>
2023-08-28 21:54:38 +0800

    新增記錄頁面篩選機制(選擇影片/完成進度/起始日期/結束日期)/簡化程式碼

--- src/client/Pages/RecordPage.jsx
+++ src/client/Pages/RecordPage.jsx
@@ -1,123 +1,213 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row, Form, Table } from 'react-bootstrap';
-import { get } from '../axios';
-import LoadingComponent from '../../components/LoadingComponent';
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row, Form, Table } from "react-bootstrap";
+import { get } from "../axios";
+import LoadingComponent from "../../components/LoadingComponent";
 
-export default function RecordPage() {
+export default function RecordPage({
+  recordType = 0,
+  currentCity = "Asia/Taipei",
+}) {
+  // 取得當前時間
+  // const isoDateInTaipei = GetCurrentDateTime();
+  const recordTypeIsPraticeOrTest = recordType === 1 ? "測驗用" : "練習用";
+
   const [loading, setLoading] = useState(false);
-  const [dataRecord, setDataRecord] = useState([]);
-  const usrToken = JSON.parse(localStorage?.getItem('user'))?.client_token;
+  const [totalVideoName, setTotalVideoName] = useState([]);
+  const [originDataRecord, setOriginDataRecord] = useState([]);
+  const [filteredDataRecord, setFilteredDataRecord] = useState([]);
+  const [filteredDataIsNull, setFilteredDataIsNull] = useState(false);
 
+  const [startRecordDate, setStartRecordDate] = useState();
+  const [endRecordDate, setEndRecordDate] = useState();
 
+  const [selectVideo, setSelectVideo] = useState("");
+  const [selectFinishChapter, setSelectFinishChapter] = useState("");
+  const [startSelectDate, setStartSelectDate] = useState("");
+  const [endSelectDate, setEndSelectDate] = useState("");
 
-  function formatDateToISODate(year, month, day) {
-    const formattedMonth = (month < 10) ? `${month}` : month;
-    const formattedDay = (day < 10) ? `${day}` : day;
-    return `${year}-${formattedMonth}-${formattedDay}`;
-  }
-  
-  const options = { timeZone: 'Asia/Taipei' };
-  const currentDate = new Date();
-  
-  const taipeiYear = currentDate.toLocaleDateString('en-US', { ...options, year: 'numeric' });
-  const taipeiMonth = currentDate.toLocaleDateString('en-US', { ...options, month: '2-digit' });
-  const taipeiDay = currentDate.toLocaleDateString('en-US', { ...options, day: '2-digit' });
-  
-  const isoDateInTaipei = formatDateToISODate(taipeiYear, taipeiMonth, taipeiDay);
-  console.log(isoDateInTaipei);
+  const usrToken = JSON.parse(localStorage?.getItem("user"))?.client_token;
+
+  useEffect(() => {
+    setLoading(true);
+    handelRecord({ api: `client/record/${usrToken}` });
+  }, [recordType]);
 
   const handelRecord = async ({ api }) => {
     const res = await get(api);
     // check the data type
     const data = res.data.data;
-    setDataRecord(data);
+    const videoName = res.data.clientVideoName;
+
+    setOriginDataRecord(data);
+    setFilteredDataRecord(data);
+
+    setTotalVideoName(videoName);
     setTimeout(() => {
       setLoading(false);
     }, 3000);
   };
+
   useEffect(() => {
-    setLoading(true);
-    handelRecord({ api: `client/record/${usrToken}` });
-  }, []);
+    if (originDataRecord.length === 0) return;
 
-  //   useEffect(() => {
-  //     console.log(dataRecord.data);
-  //   }, [dataRecord]);
+    let firstDate = originDataRecord[0].praticeDate;
+    let lastDate = originDataRecord[0].praticeDate;
 
-  //   useEffect(() => {
+    for (var i = 0; i < originDataRecord.length; i++) {
+      const currentDate = originDataRecord[i].praticeDate;
+      if (currentDate < firstDate) {
+        firstDate = currentDate;
+      }
+      if (currentDate > lastDate) {
+        lastDate = currentDate;
+      }
+    }
 
-  if (loading) return <LoadingComponent title='載入紀錄中請稍後' />;
+    const firstDateISO = new Date(firstDate).toISOString().split("T")[0];
+    const lastDateISO = new Date(lastDate).toISOString().split("T")[0];
+
+    setStartRecordDate(firstDateISO);
+    setEndRecordDate(lastDateISO);
+    setStartSelectDate(firstDateISO);
+    setEndSelectDate(lastDateISO);
+  }, [originDataRecord]);
+
+  useEffect(() => {
+    const filteredData = originDataRecord.filter((item) => {
+      const recordDate = new Date(item.praticeDate);
+      const endDate = new Date(endSelectDate);
+      endDate.setDate(endDate.getDate() + 1);
+
+      const isDateInRange =
+        recordDate >= new Date(startSelectDate) && recordDate <= endDate;
+      const isVideoMatch =
+        !selectVideo || item.clientVideoCheck === selectVideo;
+      const isChapterMatch =
+        !selectFinishChapter || item.answerState === selectFinishChapter;
+
+      return isDateInRange && isVideoMatch && isChapterMatch;
+    });
+
+    setFilteredDataRecord(filteredData);
+  }, [startSelectDate, selectFinishChapter, endSelectDate, selectVideo]);
+
+  useEffect(() => {
+    if (filteredDataRecord.length === 0) setFilteredDataIsNull(true);
+    else setFilteredDataIsNull(false);
+  }, [filteredDataRecord]);
+
+  if (loading)
+    return <LoadingComponent title={`${recordTypeIsPraticeOrTest}紀錄`} />;
 
   return (
     <Container>
       <Col>
-        <Row className='text-center'>
-          <h1>RecordPage</h1>
+        <Row className="text-center">
+          <h1>{`${recordTypeIsPraticeOrTest}紀錄`}</h1>
         </Row>
-        <Row></Row>
         <Row>
           <Col md={6}>
-            {/* <Form.Select aria-label='SelectVideo'>
-              <option>請選擇影片</option>
-              {dataVideo.map((item) => (
-                <option key={item.id} value={item.id}>
-                  {item.title}
-                </option>
-              ))}
-            </Form.Select> */}
+            <Form.Group className="mb-3" controlId="Form.SelectVideoName">
+              <Form.Label>選擇影片</Form.Label>
+              <Form.Select
+                aria-label="SelectVideo"
+                onChange={(e) => {
+                  setSelectVideo(e.target.value);
+                }}
+              >
+                <option value="">請選擇影片</option>
+                {totalVideoName.map((item, index) => (
+                  <option key={index} value={item}>
+                    {item}
+                  </option>
+                ))}
+              </Form.Select>
+            </Form.Group>
+          </Col>
+          <Col md={6}>
+            <Form.Group className="mb-3" controlId="Form.SelectFinishChapter">
+              <Form.Label>完成進度</Form.Label>
+              <Form.Select
+                aria-label="SelectFinishChapter"
+                onChange={(e) => {
+                  setSelectFinishChapter(e.target.value);
+                }}
+              >
+                <option value="">請選擇完成進度</option>
+                <option value="true">已完成</option>
+                <option value="false">未完成</option>
+              </Form.Select>
+            </Form.Group>
           </Col>
         </Row>
         <Row>
           <Col md={6}>
-            <Form.Group className='mb-3' controlId='Form.StartDate'>
+            <Form.Group className="mb-3" controlId="Form.StartDate">
               <Form.Label>起始日期</Form.Label>
               <Form.Control
-                type='date'
+                type="date"
+                name="startDate"
+                onChange={(e) => {
+                  setStartSelectDate(e.target.value);
+                }}
+                min={startRecordDate}
+                max={endRecordDate}
+                value={startSelectDate ? startSelectDate : startRecordDate}
               />
             </Form.Group>
           </Col>
           <Col md={6}>
-            <Form.Group className='mb-3' controlId='Form.EndDate'>
+            <Form.Group className="mb-3" controlId="Form.EndDate">
               <Form.Label>結束日期</Form.Label>
               <Form.Control
-                type='date'
-                name='endDate'
-                max={isoDateInTaipei}
-                value={isoDateInTaipei}
+                type="date"
+                name="endDate"
+                onChange={(e) => {
+                  setEndSelectDate(e.target.value);
+                }}
+                min={startRecordDate}
+                max={endRecordDate}
+                value={endSelectDate ? endSelectDate : endRecordDate}
               />
             </Form.Group>
           </Col>
         </Row>
-        <Row>
-          <Table striped bordered hover size='sm'>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>影片名稱</th>
-                <th>章節</th>
-                <th>是否完成</th>
-                <th>作答時間</th>
-              </tr>
-            </thead>
-            <tbody>
-              {dataRecord.map((item, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{item.clientVideoCheck}</td>
-                    <td>{item.chapter}</td>
-                    <td>{item.answerState == 'true' ? '是' : '否'}</td>
-                    <td>
-                      {new Date(item.praticeDate).toLocaleDateString() +
-                        ' ' +
-                        new Date(item.praticeDate).toLocaleTimeString()}
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </Row>
+        {filteredDataIsNull !== true ? (
+          <Row>
+            <Table striped bordered hover size="sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>影片名稱</th>
+                  <th>章節</th>
+                  <th>是否完成</th>
+                  <th>作答時間</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredDataRecord.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>{item.clientVideoCheck}</td>
+                      <td>{item.chapter}</td>
+                      <td>{item.answerState == "true" ? "是" : "否"}</td>
+                      <td>
+                        {new Date(item.praticeDate).toLocaleDateString() +
+                          " " +
+                          new Date(item.praticeDate).toLocaleTimeString()}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </Row>
+        ) : (
+          <Row className="text-center">
+            <h1>該區段查無紀錄資料</h1>
+          </Row>
+        )}
       </Col>
     </Container>
   );
