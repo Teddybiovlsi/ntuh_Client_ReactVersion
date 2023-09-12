05eca17e07eaae9501489359d2365337a15bfc3a
Teddybiovlsi <m11113005@yuntech.edu.tw>
2023-09-12 21:58:35 +0800

    修改保護路由變數名稱

--- src/RewritePasswordProtected.jsx
+++ src/RewritePasswordProtected.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 export default function RewritePasswordProtected({
-  info,
+  verifyCode,
   redirectPath = "/",
   children,
 }) {
-  if (!info) {
+  if (!verifyCode) {
     return <Navigate to={redirectPath} replace />;
   } else {
     return children ?? <Outlet />;
