5f3c01c2ba35e0311510940627f7520dfa5a4bf0
Teddybiovlsi <m11113005@yuntech.edu.tw>
2023-11-02 02:02:19 +0800

    看不出來改在哪，所以不做任何註解

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-import App from './App';
-import './styles/main.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/main.css";
 
-ReactDOM.createRoot(document.getElementById('content')).render(
+ReactDOM.createRoot(document.getElementById("content")).render(
   <React.StrictMode>
     <HashRouter>
       <App />
