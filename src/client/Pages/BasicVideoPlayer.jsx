b106f0b2259341cc53d684969ec27ac516ad691a
Teddybiovlsi <m11113005@yuntech.edu.tw>
2023-11-03 16:45:46 +0800

    新增嵌入觀看次數API

--- src/client/Pages/BasicVideoPlayer.jsx
+++ src/client/Pages/BasicVideoPlayer.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import BasicVideoJS from "../../components/BasicVideoJS";
 import "video.js/dist/video-js.css";
 import "../../components/videoqa.css";
+import { post } from "../axios";
+import LoadingComponent from "../../components/LoadingComponent";
 
 export default function BasicVideoPlayer() {
+  const user = JSON.parse(
+    localStorage.getItem("user") || sessionStorage.getItem("user")
+  );
+
   const location = useLocation();
+
+  const [loading, setLoading] = useState(true);
 
   if (!location.state) {
     alert("請先選擇影片！");
@@ -30,6 +38,38 @@ export default function BasicVideoPlayer() {
 
     playsinline: true,
   };
+
+  const fetchUploadWatchTime = async () => {
+    try {
+      const response = await post(
+        `client/addcount/video/${user.client_token}`,
+        {
+          videoID: videoID,
+        }
+      );
+      console.log("已成功上傳觀看次數");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        await fetchUploadWatchTime();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading)
+    return <LoadingComponent text="載入中..." title="基礎練習用衛教資訊" />;
 
   return (
     <>
