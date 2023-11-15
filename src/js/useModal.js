5a5317f4e58b1261b60bbd124d7a885eb9cb9c91
Teddybiovlsi <m11113005@yuntech.edu.tw>
2023-11-15 14:41:08 +0800

    新增對應的註解

--- src/js/useModal.js
+++ src/js/useModal.js
@@ -1,5 +1,13 @@
 import { useState } from "react";
 
+/**
+ * 自定義 Hook，用於管理互動視窗的顯示狀態。
+ *
+ * @returns {Array} 包含三個元素的陣列：
+ * - 第一個元素是一個布林值，表示互動視窗是否顯示。
+ * - 第二個元素是一個函數，用於關閉互動視窗。
+ * - 第三個元素是一個函數，用於打開互動視窗。
+ */
 const useModal = () => {
     const [show, setShow] = useState(false);
 
