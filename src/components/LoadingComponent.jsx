a75cf8b7a5932b2b6ba259b8534fb75075234bdb
Teddybiovlsi <m11113005@yuntech.edu.tw>
2023-11-15 15:11:45 +0800

    新增jsDoc註解

--- src/components/LoadingComponent.jsx
+++ src/components/LoadingComponent.jsx
@@ -3,6 +3,16 @@ import Loading from "./Loading";
 import { Container } from "react-bootstrap";
 import styles from "../styles/components/Common.module.scss";
 
+/**
+ * 這個 component 結合 Loading component並包含一個標題在內，主要使用於 Loading 頁面。
+ *
+ * @param {Object} props - 組件的 props。
+ * @param {string} [props.title="資訊欄位"] - 該頁面的標題。
+ * @param {string} [props.text="Loading"] - 載入中的文字。
+ *
+ * @returns {JSX.Element} 一個顯示標題與載入中文字的動畫效果。
+ */
+
 export default function LoadingComponent({
   title = "資訊欄位",
   text = "Loading",
