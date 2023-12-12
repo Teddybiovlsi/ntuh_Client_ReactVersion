2596a15c8070024d496c7c7125b0205d1a3767f8
Teddybiovlsi <M11113005@yuntech.edu.tw>
2023-12-12 22:48:51 +0800

    修改Row的樣式+以role形式使整個row都能被點選

--- src/client/Pages/BasicVideoList.jsx
+++ src/client/Pages/BasicVideoList.jsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from "react";
-import { useState, useEffect } from "react";
+import React, { useMemo } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Container,
   Row,
@@ -8,24 +8,24 @@ import {
   Modal,
   Card,
   Image,
-} from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
-import { get } from "../axios";
-import BtnBootstrap from "../../components/BtnBootstrap";
-import LoadingComponent from "../../components/LoadingComponent";
-import { AiFillLock } from "react-icons/ai";
-import styles from "../../styles/pages/VideoList.module.scss";
-import useModal from "../../js/useModal";
-import { ProgressBar } from "react-bootstrap";
+} from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
+import { get } from '../axios';
+import BtnBootstrap from '../../components/BtnBootstrap';
+import LoadingComponent from '../../components/LoadingComponent';
+import { AiFillLock } from 'react-icons/ai';
+import styles from '../../styles/pages/VideoList.module.scss';
+import useModal from '../../js/useModal';
+import { ProgressBar } from 'react-bootstrap';
 import {
   CircularProgressbar,
   CircularProgressbarWithChildren,
   buildStyles,
-} from "react-circular-progressbar";
-import { IoIosArrowForward } from "react-icons/io";
-import { FaLock } from "react-icons/fa";
-import "react-circular-progressbar/dist/styles.css";
-import PageTitleHeading from "../../components/PageTitleHeading";
+} from 'react-circular-progressbar';
+import { IoIosArrowForward } from 'react-icons/io';
+import { FaLock } from 'react-icons/fa';
+import 'react-circular-progressbar/dist/styles.css';
+import PageTitleHeading from '../../components/PageTitleHeading';
 
 /**
  * 基本練習影片列表
@@ -34,7 +34,7 @@ import PageTitleHeading from "../../components/PageTitleHeading";
  * @returns {JSX.Element} 顯示基本練習衛教資訊的頁面
  */
 
-export default function BasicVideoList({ loadingText = "資訊載入中", user }) {
+export default function BasicVideoList({ loadingText = '資訊載入中', user }) {
   const convertTheWatchTimePercentage = ({
     videoDuration,
     videoLastWatchTime,
@@ -66,7 +66,7 @@ export default function BasicVideoList({ loadingText = "資訊載入中", user }
     showConfirmToLatestTimeModal,
   ] = useModal();
 
-  const checkIsClient = user?.permission === "ylhClient";
+  const checkIsClient = user?.permission === 'ylhClient';
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -117,20 +117,20 @@ export default function BasicVideoList({ loadingText = "資訊載入中", user }
     } catch (error) {
       const errorMessage = error.response.data.message;
 
-      if (errorMessage === "發生錯誤，請重新登入") {
+      if (errorMessage === '發生錯誤，請重新登入') {
         clearUserSession();
 
         alert(errorMessage);
-        navigate("/");
+        navigate('/');
       }
-      if (errorMessage === "影片Index錯誤，請重新嘗試") {
-        alert("影片有新版本，請重新登入");
+      if (errorMessage === '影片Index錯誤，請重新嘗試') {
+        alert('影片有新版本，請重新登入');
         clearUserSession();
-        navigate("/", { replace: true });
+        navigate('/', { replace: true });
       } else {
-        alert("發生不明錯誤，請重新登入");
+        alert('發生不明錯誤，請重新登入');
         clearUserSession();
-        navigate("/Home", { replace: true });
+        navigate('/Home', { replace: true });
       }
     }
   };
@@ -155,20 +155,20 @@ export default function BasicVideoList({ loadingText = "資訊載入中", user }
   const checkHavePermissionToTest = () => {
     if (open !== null) {
       if (
-        (open.learningProgress === 100 && user?.permission === "ylhClient") ||
-        user?.permission === "ylhGuest"
+        (open.learningProgress === 100 && user?.permission === 'ylhClient') ||
+        user?.permission === 'ylhGuest'
       ) {
-        navigate("/basic/videoQuestion", {
+        navigate('/basic/videoQuestion', {
           state: {
             videoID: open.videoCertainID,
             info: open.QuestionData,
           },
         });
       } else {
-        alert("請先觀看完影片再進行測驗");
+        alert('請先觀看完影片再進行測驗');
       }
     } else {
-      alert("請先觀看完影片再進行測驗");
+      alert('請先觀看完影片再進行測驗');
     }
   };
 
@@ -178,210 +178,221 @@ export default function BasicVideoList({ loadingText = "資訊載入中", user }
   if (arrayIsEmpty) {
     return (
       <Container>
-        <h1 className="text-center">{title}</h1>
-        <h2 className="m-3 p-3 text-center">{`沒有對應的${title}`}</h2>
+        <h1 className='text-center'>{title}</h1>
+        <h2 className='m-3 p-3 text-center'>{`沒有對應的${title}`}</h2>
       </Container>
     );
   }
   return (
     <>
-      <PageTitleHeading text="基本練習衛教資訊" styleOptions={4} />
-      <Container>
+      <PageTitleHeading text='基本練習衛教資訊' styleOptions={4} />
+      <Container className='pb-5'>
         {originVideoData.map((video, eachQuestionIndex) => {
           return (
             <>
-              <p className="fs-4 ps-0 m-0">{video.Title}</p>
-              <Row className={styles.videoListContainer}>
-                <Col xs={10} md={11} lg={11}>
-                  <Row>
-                    <Image src={video.image_url} className="p-0" rounded />
-                  </Row>
-                  {checkIsClient && (
-                    <Row className={styles.learningProgressCol}>
-                      <Col
-                        xs={3}
-                        md={2}
-                        lg={2}
-                        className="d-flex align-items-center justify-content-center"
-                      >
-                        <CircularProgressbar
-                          value={video.learningProgress}
-                          text={`${video.learningProgress}%`}
-                          styles={buildStyles({
-                            textColor:
-                              video.learningProgress === 100
-                                ? "#00A3A3"
-                                : "#FFD4D4",
-                            pathColor:
-                              video.learningProgress === 100
-                                ? "#00A3A3"
-                                : "#940000",
-                            trailColor:
-                              video.learningProgress === 100
-                                ? "#F2FFFF"
-                                : "#FFD4D4",
-                          })}
-                        />
-                      </Col>
-                      <Col xs={9} md={10} lg={10} className={`fs-3 `}>
-                        <p className={`m-0 ${styles.learningProgressFont}`}>
-                          學習進度：
-                        </p>
-                        <p
-                          className={`m-0 ${
-                            video.learningProgress === 100
-                              ? styles.learningProgressProgressSuccess
-                              : "text-danger"
-                          }`}
+              <Row
+                xs={12}
+                key={Math.random()}
+                onClick={() => {
+                  setOpen(video);
+                }}
+              >
+                <Col xs={12} lg={{ span: 8, offset: 3 }}>
+                  <p className='fs-4 ps-0 m-0'>{video.Title}</p>
+                </Col>
+              </Row>
+              <Row
+                role='button'
+                onClick={() => {
+                  setOpen(video);
+                }}
+              >
+                <Row className={styles.videoListContainer}>
+                  <Col xs={10} md={11} lg={{ span: 6, offset: 3 }}>
+                    <Row>
+                      <Image src={video.image_url} className='p-0' rounded />
+                    </Row>
+                    {checkIsClient && (
+                      <Row className={styles.learningProgressCol}>
+                        <Col
+                          xs={3}
+                          md={2}
+                          lg={2}
+                          className='d-flex align-items-center justify-content-center'
                         >
-                          {video.learningProgress}%
+                          <CircularProgressbar
+                            value={video.learningProgress}
+                            text={`${video.learningProgress}%`}
+                            styles={buildStyles({
+                              textColor:
+                                video.learningProgress === 100
+                                  ? '#00A3A3'
+                                  : '#FFD4D4',
+                              pathColor:
+                                video.learningProgress === 100
+                                  ? '#00A3A3'
+                                  : '#940000',
+                              trailColor:
+                                video.learningProgress === 100
+                                  ? '#F2FFFF'
+                                  : '#FFD4D4',
+                            })}
+                          />
+                        </Col>
+                        <Col xs={9} md={10} lg={10} className={`fs-3 `}>
+                          <p className={`m-0 ${styles.learningProgressFont}`}>
+                            學習進度：
+                          </p>
+                          <p
+                            className={`m-0 ${
+                              video.learningProgress === 100
+                                ? styles.learningProgressProgressSuccess
+                                : 'text-danger'
+                            }`}
+                          >
+                            {video.learningProgress}%
+                          </p>
+                        </Col>
+                      </Row>
+                    )}
+                    {!checkIsClient && (
+                      <Row className={styles.guestLearningProgressCol}>
+                        <p className='m-0 fs-4'>
+                          <FaLock />
+                          <br />
+                          權限不足無法訪問
                         </p>
-                      </Col>
-                    </Row>
-                  )}
-                  {!checkIsClient && (
-                    <Row className={styles.guestLearningProgressCol}>
-                      <p className="m-0 fs-4">
-                        <FaLock />
-                        <br />
-                        權限不足無法訪問
-                      </p>
-                    </Row>
-                  )}
-                </Col>
-                <Col
-                  xs={2}
-                  md={1}
-                  lg={1}
-                  className={styles.linkToStartLearning}
-                  as="button"
-                  onClick={() => {
-                    setOpen(video);
-                  }}
-                >
-                  <h1>
-                    <IoIosArrowForward />
-                  </h1>
-                </Col>
+                      </Row>
+                    )}
+                  </Col>
+                  <Col
+                    xs={2}
+                    md={1}
+                    lg={1}
+                    className={styles.linkToStartLearning}
+                  >
+                    <h1>
+                      <IoIosArrowForward />
+                    </h1>
+                  </Col>
+                </Row>
               </Row>
             </>
           );
         })}
       </Container>
-      <Container>
-        <Modal show={open !== null} onHide={handleCloseStartModal}>
-          <Modal.Header closeButton>
-            <Modal.Title>請選擇欲觀看的類型</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Container>
-              <Stack gap={3}>
+      <Modal show={open !== null} onHide={handleCloseStartModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>請選擇欲觀看的類型</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Stack gap={3}>
+              <BtnBootstrap
+                onClickEventName={() => {
+                  // 若有保留觀看進度，則跳出確認視窗
+                  if (open !== null && open.videoLastestTime > 0) {
+                    showConfirmToLatestTimeModal();
+                  }
+                  // 若沒有保留觀看進度，則直接跳轉到影片播放頁面
+                  else {
+                    navigate('/Basicvideo', {
+                      state: {
+                        info: open.QuestionData,
+                        videoPath: open.video_url,
+                        videoID: open.videoCertainID,
+                        latestWatchTime: 0,
+                      },
+                    });
+                  }
+                }}
+                text={`開始基本練習`}
+                variant={'outline-primary'}
+              />
+              {open !== null && open.QuestionData.length !== 0 && (
                 <BtnBootstrap
-                  onClickEventName={() => {
-                    // 若有保留觀看進度，則跳出確認視窗
-                    if (open !== null && open.videoLastestTime > 0) {
-                      showConfirmToLatestTimeModal();
-                    }
-                    // 若沒有保留觀看進度，則直接跳轉到影片播放頁面
-                    else {
-                      navigate("/Basicvideo", {
-                        state: {
-                          info: open.QuestionData,
-                          videoPath: open.video_url,
-                          videoID: open.videoCertainID,
-                          latestWatchTime: 0,
-                        },
-                      });
-                    }
-                  }}
-                  text={`開始基本練習`}
-                  variant={"outline-primary"}
+                  text={
+                    open !== null &&
+                    (open.learningProgress === 100 ||
+                    user?.permission === 'ylhGuest' ? (
+                      `題目測驗`
+                    ) : (
+                      <p className='text-danger m-0 p-0'>
+                        <AiFillLock />
+                        鎖定中
+                      </p>
+                    ))
+                  }
+                  variant={
+                    open !== null &&
+                    (open.learningProgress === 100 || !checkIsClient)
+                      ? 'outline-primary'
+                      : 'outline-danger'
+                  }
+                  onClickEventName={checkHavePermissionToTest}
+                  disabled={
+                    checkIsClient &&
+                    (open === null || open.learningProgress !== 100)
+                  }
                 />
-                {open !== null && open.QuestionData.length !== 0 && (
-                  <BtnBootstrap
-                    text={
-                      open !== null &&
-                      (open.learningProgress === 100 ||
-                      user?.permission === "ylhGuest" ? (
-                        `題目測驗`
-                      ) : (
-                        <p className="text-danger m-0 p-0">
-                          <AiFillLock />
-                          鎖定中
-                        </p>
-                      ))
-                    }
-                    variant={
-                      open !== null &&
-                      (open.learningProgress === 100 || !checkIsClient)
-                        ? "outline-primary"
-                        : "outline-danger"
-                    }
-                    onClickEventName={checkHavePermissionToTest}
-                    disabled={
-                      checkIsClient &&
-                      (open === null || open.learningProgress !== 100)
-                    }
-                  />
-                )}
-              </Stack>
-            </Container>
+              )}
+            </Stack>
+          </Container>
+        </Modal.Body>
+      </Modal>
+      {/* 若有保留觀看紀錄，Modal */}
+      {checkIsClient && (
+        <Modal
+          show={isConfirmingToLatestTime}
+          onHide={closeIsConfirmingToLatestTime}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>請確認</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>
+              您上次觀看影片的進度為
+              <b className='text-primary'>
+                {open !== null && Math.round(open.videoLastestTime * 10) / 10}
+              </b>
+              秒，是否繼續觀看？
+            </p>
+            <p className='text-danger'>若選擇取消，則觀看進度將會重置為0秒</p>
           </Modal.Body>
+
+          <Modal.Footer>
+            <BtnBootstrap
+              text={`取消`}
+              variant={'outline-secondary'}
+              onClickEventName={() => {
+                navigate('/Basicvideo', {
+                  state: {
+                    info: open.QuestionData,
+                    videoPath: open.video_url,
+                    videoID: open.videoCertainID,
+                    latestWatchTime: 0,
+                  },
+                });
+              }}
+            />
+            <BtnBootstrap
+              text={`確認`}
+              variant={'outline-primary'}
+              onClickEventName={() => {
+                closeIsConfirmingToLatestTime();
+                navigate('/Basicvideo', {
+                  state: {
+                    videoPath: open.video_url,
+                    videoID: open.videoCertainID,
+                    latestWatchTime: open.videoLastestTime,
+                  },
+                });
+              }}
+            />
+          </Modal.Footer>
         </Modal>
-        {/* 若有保留觀看紀錄，Modal */}
-        {checkIsClient && (
-          <Modal
-            show={isConfirmingToLatestTime}
-            onHide={closeIsConfirmingToLatestTime}
-          >
-            <Modal.Header closeButton>
-              <Modal.Title>請確認</Modal.Title>
-            </Modal.Header>
-
-            <Modal.Body>
-              <p>
-                您上次觀看影片的進度為
-                <b className="text-primary">
-                  {open !== null && Math.round(open.videoLastestTime * 10) / 10}
-                </b>
-                秒，是否繼續觀看？
-              </p>
-              <p className="text-danger">若選擇取消，則觀看進度將會重置為0秒</p>
-            </Modal.Body>
-
-            <Modal.Footer>
-              <BtnBootstrap
-                text={`取消`}
-                variant={"outline-secondary"}
-                onClickEventName={() => {
-                  navigate("/Basicvideo", {
-                    state: {
-                      info: open.QuestionData,
-                      videoPath: open.video_url,
-                      videoID: open.videoCertainID,
-                      latestWatchTime: 0,
-                    },
-                  });
-                }}
-              />
-              <BtnBootstrap
-                text={`確認`}
-                variant={"outline-primary"}
-                onClickEventName={() => {
-                  closeIsConfirmingToLatestTime();
-                  navigate("/Basicvideo", {
-                    state: {
-                      videoPath: open.video_url,
-                      videoID: open.videoCertainID,
-                      latestWatchTime: open.videoLastestTime,
-                    },
-                  });
-                }}
-              />
-            </Modal.Footer>
-          </Modal>
-        )}
-      </Container>
+      )}
     </>
   );
 }
